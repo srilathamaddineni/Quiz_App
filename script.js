e2732b6d756fd27a5371eefdc06101f3da5a53d0
srilathamaddineni <srilatha.maddineni17@gmail.com>
2023-10-13 23:38:08 -0400

    Adding files

---
+++ script.js
@@ -0,0 +1,100 @@
+const startButton=document.getElementById("start-btn");
+const questionBlock=document.getElementById("question-container");
+const nextButton=document.getElementById("nxt-btn");
+const qtn=document.getElementById("question");
+const answerButtons=document.getElementById("answer-buttons");
+let currentIndex;
+startButton.addEventListener('click',startGame);
+nextButton.addEventListener('click',()=>{
+     currentIndex++;
+     clearStatus();
+     showQuestion(questions);
+})
+
+function startGame(){
+  startButton.classList.add('hide');
+  questionBlock.classList.remove('hide');
+  currentIndex=0;
+}
+function setNextQuestion(){
+    resetState();
+    showQuestion(questions);
+}
+
+function showQuestion(questions){
+    if(currentIndex>questions.length-2)
+    {
+        nextButton.classList.add('hide');
+    }
+   qtn.innerText=questions[currentIndex].question;
+   questions[currentIndex].answers.forEach(answer=>{
+      const button=document.createElement('button');
+      button.innerText=answer.text;
+      button.classList.add('btn');
+      if(answer.correct)
+      {
+         button.dataset.correct=answer.correct;
+      }
+      button.addEventListener('click',selectAnswer);
+      answerButtons.appendChild(button);
+      
+   })
+}
+function resetState(){
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
+    while(answerButtons.firstChild)
+    {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+
+}
+function selectAnswer(e){
+    const selectedButton=e.target;
+    const correct=selectedButton.dataset.correct;
+    setStatusClass(document.body,correct);
+    Array.from(answerButtons.children).forEach(button=>{
+        setStatusClass(button,button.dataset.correct);
+    })
+}
+function setStatusClass(element,correct){
+   clearStatus(element);
+   if(correct){
+     element.classList.add('correct');
+   }
+   else{
+    element.classList.add('wrong');
+   }
+}
+function clearStatus(element){
+    answerButtons.innerHTML='';
+}
+const questions=[
+    {
+      question:'Who is prime minister of India',
+      answers:[
+          {text:'Modi', correct:true},
+          {text:'Kalam',correct:false},
+          {text:'singh', correct:false},
+          {text:'soniya', correct:false}
+      ]
+    },
+    {
+        question:'what is the best programming language',
+        answers:[
+            {text:'C++', correct:false},
+            {text:'Java', correct:true},
+            {text:'Python', correct:false},
+            {text:'javascript', correct:false}
+        ]
+    },
+    {
+        question:'What is the most used social media',
+        answers:[
+            {text:'LinkedIn', correct:false},
+            {text:'Instagram', correct:true},
+            {text:'Facebook', correct:false},
+            {text:'Twitter', correct:false}
+        ]
+    }
+  ]
